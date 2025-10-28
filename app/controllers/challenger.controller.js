82f88fbed53ebf4198763d672a8f57a2b0d8b45c
Abhishek Mishra <abhishek.mishra@icubeswire.com>
2025-10-28 16:13:31 +0530

    counter fix

--- app/controllers/challenger.controller.js
+++ app/controllers/challenger.controller.js
@@ -454,10 +454,10 @@ exports.getEngagement = async (req, res) => {
 
         let challengerCount = await Challenger.countDocuments({
             isDeleted: false,
-            // $or: [
-            //     { otpVerified: { $eq: true } },
-            //     { isPrevious: { $eq: true } }
-            // ]
+            $or: [
+                { otpVerified: { $eq: true } },
+                { isPrevious: { $eq: true } }
+            ]
         });
         let challengerProgress = await challangerProgress
             .findOne({ name: "challenge" })
