0b1dc748bb392f29a0559fcb7fccf062b4c7a06c
Abhishek Mishra <abhishek.mishra@icubeswire.com>
2025-10-30 16:09:33 +0530

    otp hide and auth

--- app/controllers/challenger.controller.js
+++ app/controllers/challenger.controller.js
@@ -26,7 +26,7 @@ exports.listAdmin = async (req, res) => {
         if (req.query.category) filter.category = req.query.category;
         if (req.query.subcategory) filter.subcategory = req.query.subcategory;
         const records = await Challenger.find(filter,{name:1,mobile:1,duration:1,category:1,subcategory:1,type:1,pdf:1,createdAt:1})
-            .sort({ createdAt: -1 ,otpVerified:-1})
+            .sort({ otpVerified:-1, createdAt: -1})
             .skip(skip)
             .limit(limit);
 
